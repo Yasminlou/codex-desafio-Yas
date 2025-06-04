b00e33e3c0298d18097cdce4cea45cad81257db3
Yasmin Loureiro <yasmin.lou@hotmail.com>
2025-06-03 22:05:51 -0300

    feat: adjusting inset ads

--- front/src/utils/insertAds.js
+++ front/src/utils/insertAds.js
@@ -1,12 +1,20 @@
-const insertAds = (items) => {
-    const result = [];
-    items.forEach((item, index) => {
-      result.push(item);
-      if ((index + 1) % 8 === 0) {
-        result.push({ type: 'ad' });
+export default function insertAds(feedItems) {
+  const result = [];
+  let contentCount = 0;
+
+  feedItems.forEach((item, index) => {
+    result.push(item);
+
+    // Só conta como conteúdo se não for agrupador
+    if (item.type !== 'agrupador-materia') {
+      contentCount++;
+
+      // A cada 8 conteúdos, insere um anúncio
+      if (contentCount % 8 === 0) {
+        result.push({ type: 'ad', id: `ad-${contentCount}` });
       }
-    });
-    return result;
-  };
-  
-  export default insertAds;
+    }
+  });
+
+  return result;
+}
